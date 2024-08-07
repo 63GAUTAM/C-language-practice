26ddc762f7d0ba4ee202f8aa650a5c2a4f243764
Gautam surolia <gautamsurolia63@gmail.com>
2024-08-07 21:00:16 +0530

    Add files via upload

---
+++ Chapter 1 Practice set/04_problem.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    float p = 34.1;
+    int r = 5;
+    int t = 7;
+    printf("The value of simple interest is %f", (p*r*t)/100);
+    return 0;
+}
---
+++ Chapter 1 Practice set/1_problem.c
@@ -0,0 +1,23 @@
+// #include<stdio.h>
+
+// int main(){
+//     int length = 10;
+//     int breadth = 10;
+//     printf("The area of rectangle is %d", length*breadth);
+//     return 0;
+// }
+
+
+#include<stdio.h>
+
+int main(){
+    int l,b;
+    printf("Enter Length\n ");
+    scanf("%d", &l);
+
+    printf("Enter breath\n");
+    scanf("%d", &b);
+
+    printf("The area of rectangle is %d", l*b);
+    return 0;
+}
